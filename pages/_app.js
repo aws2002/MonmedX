55e536201fddab0b3fdbd5a401aba86124086e5e
Osama Husam <otaku3game@gmail.com>
2022-09-11 09:33:12 +0300

    un slick-carousel

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 // import Layout from "../components/Layouts/Layout";
 import "../styles/main.scss";
 import "../components/Skeleton/skeleton.scss"
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 export default function MyApp({ Component, pageProps }) {
   const Layout=Component.Layout || EmptyLayout
   return (
